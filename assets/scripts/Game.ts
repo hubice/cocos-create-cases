314604d3268033e843fa727df3598f8e437e97c3
horizon <1820120473@qq.com>
2021-02-02 01:48:43 +0800

    封装操作

--- assets/scripts/Game.ts
+++ assets/scripts/Game.ts
@@ -1,50 +1,17 @@
 import Global from "./common/Global";
+import Operations from "./service/Operations";
 
 const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class Game extends cc.Component {
     onLoad() {
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        Operations.start()
     }
     onDestroy() {
-        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        Operations.end()
     }
-    onKeyDown(event) {
-        switch(event.keyCode) {
-            case cc.macro.KEY.w:
-                Global.input.direction.y = 1
-                break;
-            case cc.macro.KEY.s:
-                Global.input.direction.y = -1
-                break;
-            case cc.macro.KEY.a:
-                Global.input.direction.x = -1
-                break;
-            case cc.macro.KEY.d:
-                Global.input.direction.x = 1
-                break;
-        }
-    }
-    onKeyUp(event) {
-        switch(event.keyCode) {
-            case cc.macro.KEY.w:
-                Global.input.direction.y = 0
-                break;
-            case cc.macro.KEY.s:
-                Global.input.direction.y = 0
-                break;
-            case cc.macro.KEY.a:
-                Global.input.direction.x = 0
-                break;
-            case cc.macro.KEY.d:
-                Global.input.direction.x = 0
-                break;
-        }
-    }
-    update(dt) {
+    update() {
         Global.frame++
         if (Global.frame % 30 == 0) {
             console.log(Global.input.direction)
--- assets/scripts/service/Operations.ts
+++ assets/scripts/service/Operations.ts
@@ -1,3 +1,47 @@
+import Global from "../common/Global";
+
+/**
+ * 键盘操作监听
+ */
 export default class Operations {
-    
+    static start() {
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, Operations.onKeyDown, Operations);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, Operations.onKeyUp, Operations);
+    };
+    static end() {
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, Operations.onKeyDown, Operations);
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, Operations.onKeyUp, Operations);
+    };
+    static onKeyDown(event) {
+        switch (event.keyCode) {
+            case cc.macro.KEY.w:
+                Global.input.direction.y = 1
+                break;
+            case cc.macro.KEY.s:
+                Global.input.direction.y = -1
+                break;
+            case cc.macro.KEY.a:
+                Global.input.direction.x = -1
+                break;
+            case cc.macro.KEY.d:
+                Global.input.direction.x = 1
+                break;
+        }
+    };
+    static onKeyUp(event) {
+        switch (event.keyCode) {
+            case cc.macro.KEY.w:
+                Global.input.direction.y = 0
+                break;
+            case cc.macro.KEY.s:
+                Global.input.direction.y = 0
+                break;
+            case cc.macro.KEY.a:
+                Global.input.direction.x = 0
+                break;
+            case cc.macro.KEY.d:
+                Global.input.direction.x = 0
+                break;
+        }
+    }
 }
